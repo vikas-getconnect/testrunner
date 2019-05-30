6c859acef367a17655edef911417e3b054d7bb37
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-05-30 14:24:30 +0530

    Adding try catch

--- pytests/eventing/handler_code/n1ql_op_without_timers.js
+++ pytests/eventing/handler_code/n1ql_op_without_timers.js
@@ -1,21 +1,23 @@
 function OnUpdate(doc, meta) {
     var docID= meta.id;
-    // Adding these extra comments to validate MB-30240
-    log('Before Inserting document', doc);
-    log('Before Inserting document', doc);
-    log('Before Inserting document', doc);
+    try {
     var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'N1QL op');
-    // Adding these extra comments to validate MB-30240
-    log('After Inserting document', doc);
-    log('After Inserting document', doc);
-    log('After Inserting document', doc);
+    } catch (e) {
+        log(e);
+        }
+    }
 }
 
 function OnDelete(meta) {
     var docID = meta.id;
     // Adding these extra comments to validate MB-30240
     log('Deleting document', docID);
+    try{
     var query = DELETE FROM dst_bucket where meta().id = $docID;
+    } catch (e) {
+        log(e);
+        }
+    }
     // Adding these extra comments to validate MB-30240
     log('Deleting document', docID);
 }
